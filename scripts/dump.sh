9814bd79c39bf1773581fb28d1c09cca08f302e6
Daniel Reigada <daniel.reigada@gmail.com>
2023-08-22 15:55:09 +0100

    add logs

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 
 DUMP_FILE=/dump.sql
 
+echo "Starting dump"
+
 pg_dump -v --no-owner --no-privileges --clean --if-exists --quote-all-identifiers "$OLD_DATABASE_URL" -f "$DUMP_FILE"
+
+echo "Dump finished"
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 
 DUMP_FILE=/dump.sql
 
+echo "Starting restore"
+
 psql "$NEW_DATABASE_URL" -f "$DUMP_FILE"
+
+echo "Restore finished"
